375b9be50ff7cc5bad541a513f2fe09409032642
lukai <lukai@knowbox.cn>
2019-03-11 11:38:13 +0800

    add api<27 fragement leakcanary

---
+++ src/main/java/com/squareup/leakcanary/SupportFragmentRefWatcher.java
@@ -0,0 +1,37 @@
+package com.squareup.leakcanary;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+
+public class SupportFragmentRefWatcher implements FragmentRefWatcher {
+    private final RefWatcher refWatcher;
+    public SupportFragmentRefWatcher(RefWatcher refWatcher) {
+        this.refWatcher = refWatcher;
+    }
+
+    private final FragmentManager.FragmentLifecycleCallbacks fragmentLifecycleCallbacks =
+            new FragmentManager.FragmentLifecycleCallbacks() {
+
+                @Override public void onFragmentViewDestroyed(FragmentManager fm, Fragment fragment) {
+                    View view = fragment.getView();
+                    if (view != null) {
+                        refWatcher.watch(view);
+                    }
+                }
+
+                @Override public void onFragmentDestroyed(FragmentManager fm, Fragment fragment) {
+                    refWatcher.watch(fragment);
+                }
+            };
+
+    @Override public void watchFragments(Activity activity) {
+        if (activity instanceof FragmentActivity) {
+            FragmentManager supportFragmentManager =
+                    ((FragmentActivity) activity).getSupportFragmentManager();
+            supportFragmentManager.registerFragmentLifecycleCallbacks(fragmentLifecycleCallbacks, true);
+        }
+    }
+}
